f992b0f4cd0bd0dbb146f4635dba8e60252da930
Neil Hemnani <hemnanineil@gmail.com>
2020-04-08 01:45:56 -0400

    added code for key event handler

--- Editor.cpp
+++ Editor.cpp
@@ -1,13 +1,49 @@
 #include "Editor.h"
+#include <iostream>
+
+using namespace std;
 
 Editor::Editor() {
-    ECTextViewImp wndTest;
-    wndTest.AddRow("CSE 3150");
-    wndTest.AddRow("This is a very simple demo of the ECTextViewImp functionalities.");
-    wndTest.AddRow("Press ctrl-q to quit");    
-    wndTest.Show();
+    numRows = 0;
+    ECTextViewImp wnd;
+    KeyHandler kh = KeyHandler(wnd);
+    wnd.Attach(this); 
+    wnd.AddStatusRow("left", "right", false);
+    
+    this->AddLine("CSE 3150");
+    this->AddLine("This is a very simple demo of the ECTextViewImp functionalities.");
+    this->AddLine("Press ctrl-q to quit");
+    for (auto line : text)
+        wnd.AddRow(line);
+
+    wnd.Show();
 }
 
+/* KEY ENUMERATION FOR REFERENCE
+    TAB = 9,
+    ENTER = 13,
+    CTRL_Q = 17,
+    CTRL_Y = 25,
+    CTRL_Z = 26,
+    ESC = 27,
+    BACKSPACE =  127,
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+  KEY ENUMERATION FOR REFERENCE */
+
 void Editor::Update() {
-    ;
+    keyPressed = wnd.GetPressedKey();
+    kh.Handle(keyPressed);
+    wnd.Refresh();
+}
+
+void Editor::AddLine(std::string line) {
+    text.push_back(line);
+    numRows += 1;
+}
+
+bool KeyHandler::Handle(int keyPressed) {
+    return true;
 }
--- Editor.h
+++ Editor.h
@@ -1,11 +1,29 @@
 #include "ECTextViewImp.h"
 
+// first key handler: will chain down to other key handlers
+class KeyHandler {
+public:
+    KeyHandler(ECTextViewImp& wnd) : wnd(wnd) {}
+    ~KeyHandler() {}
+
+    virtual bool Handle(int keyPressed);
+private:
+    ECTextViewImp &wnd;
+};
+
+// main observer of ECTextViewImp
 class Editor : public ECObserver {
 public:
     Editor();
     ~Editor() {}
 
     void Update();
+    
+    void AddLine(std::string line);
 private:
-    ECTextViewImp wndTest;
+    ECTextViewImp wnd;
+    KeyHandler kh = KeyHandler(wnd);
+    std::vector<std::string> text;
+    int numRows;
+    int keyPressed;
 };
