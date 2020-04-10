2df7181bd3730865dd591e3166d1332ca85f46ef
Neil Hemnani <hemnanineil@gmail.com>
2020-04-10 18:59:36 -0400

    fixed bug to make Update() work, and started working on handling arrow keys

--- Editor.cpp
+++ Editor.cpp
@@ -1,49 +1,58 @@
 #include "Editor.h"
+#include "ECCommand.h"
 #include <iostream>
 
 using namespace std;
 
 Editor::Editor() {
     numRows = 0;
-    ECTextViewImp wnd;
-    KeyHandler kh = KeyHandler(wnd);
-    wnd.Attach(this); 
-    wnd.AddStatusRow("left", "right", false);
+    wnd.AddStatusRow("", "", false);
     
     this->AddLine("CSE 3150");
     this->AddLine("This is a very simple demo of the ECTextViewImp functionalities.");
     this->AddLine("Press ctrl-q to quit");
-    for (auto line : text)
+    for (auto line : text) {
         wnd.AddRow(line);
+        numRows += 1;
+    }
 
+    wnd.SetCursorX(1);
+    wnd.SetCursorY(1);
+    wnd.Attach(this); 
     wnd.Show();
 }
 
-/* KEY ENUMERATION FOR REFERENCE
-    TAB = 9,
-    ENTER = 13,
-    CTRL_Q = 17,
-    CTRL_Y = 25,
-    CTRL_Z = 26,
-    ESC = 27,
-    BACKSPACE =  127,
-    ARROW_LEFT = 1000,
-    ARROW_RIGHT,
-    ARROW_UP,
-    ARROW_DOWN
-  KEY ENUMERATION FOR REFERENCE */
-
 void Editor::Update() {
     keyPressed = wnd.GetPressedKey();
-    kh.Handle(keyPressed);
-    wnd.Refresh();
+    if (keyPressed >= 1000 && keyPressed <= 1003) {
+        ArrowHandle(keyPressed);
+    }
+}
+
+void Editor::ArrowHandle(int keyPressed) {
+    cX = wnd.GetCursorX();
+    cY = wnd.GetCursorY();
+
+    if (keyPressed == 1000) {           // left
+        if (cX > 0) {
+            cX -= 1;
+            wnd.SetCursorX(cX);
+        } else if (cX == 0 && cY > 0) {
+            cY -= 1;
+            cX = (int)text[cY].size();
+            wnd.SetCursorX(cX);
+            wnd.SetCursorY(cY);
+        }
+    } else if (keyPressed == 1001) {    // right
+
+    } else if (keyPressed == 1002) {    // up
+        
+    } else if (keyPressed == 1003) {    // down
+        
+    }
 }
 
 void Editor::AddLine(std::string line) {
     text.push_back(line);
     numRows += 1;
-}
-
-bool KeyHandler::Handle(int keyPressed) {
-    return true;
 }
--- Editor.h
+++ Editor.h
@@ -1,15 +1,14 @@
 #include "ECTextViewImp.h"
 
-// first key handler: will chain down to other key handlers
-class KeyHandler {
-public:
-    KeyHandler(ECTextViewImp& wnd) : wnd(wnd) {}
-    ~KeyHandler() {}
-
-    virtual bool Handle(int keyPressed);
-private:
-    ECTextViewImp &wnd;
-};
+/* KEY ENUMERATION FOR REFERENCE
+    TAB = 9,
+    ENTER = 13,
+    CTRL_Q = 17,
+    CTRL_Y = 25,
+    CTRL_Z = 26,
+    ESC = 27,
+    BACKSPACE =  127
+  KEY ENUMERATION FOR REFERENCE */
 
 // main observer of ECTextViewImp
 class Editor : public ECObserver {
@@ -18,12 +17,15 @@ public:
     ~Editor() {}
 
     void Update();
+    void ArrowHandle(int keyPressed);
     
     void AddLine(std::string line);
+    
 private:
     ECTextViewImp wnd;
-    KeyHandler kh = KeyHandler(wnd);
     std::vector<std::string> text;
     int numRows;
+    int cX;
+    int cY;
     int keyPressed;
 };
